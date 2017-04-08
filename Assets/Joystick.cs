912f355bbe0f6a51d923e1e7624cb13deb7c38aa
Stephen Furlani <stephen.furlani@gmail.com>
2017-04-07 22:21:02 -0400

    initial commit - have it working as sprites but weird UI artifacts

---
+++ Assets/Joystick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour {
+
+	public float dx = 0.5f;
+	public float dy = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		float x = pos.x + (Input.GetKeyUp(KeyCode.RightArrow)?dx:0) + (Input.GetKeyUp(KeyCode.LeftArrow)?-dx:0);
+		float y = pos.y + (Input.GetKeyUp(KeyCode.UpArrow)?dy:0) + (Input.GetKeyUp(KeyCode.DownArrow)?-dy:0);
+		transform.position = new Vector3(x, y, pos.z);
+	}
+}
---
+++ Assets/PixelPerfect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfect : MonoBehaviour {
+
+	public float scale = 1.0f;
+	public float PPU = 16.0f;
+
+	// Use this for initialization
+	void Start () {
+		// Camera Size = x / ((( x / y ) * 2 ) * s )
+		Camera camera = GetComponent<Camera>();
+		var x = (float)Screen.width;
+		var y = (float)Screen.height;
+		var s = PPU * scale;
+		camera.orthographicSize = x / ((( x / y ) * 2.0f ) * s );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
